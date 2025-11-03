0f5776ce949d04c3018ac467d3acb0513f364e88
Your Name <your.email@example.com>
2025-11-03 11:05:41 +0530

    added home as default

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
-import DashboardPage from "./pages/DashboardPage";
+import HomePage from "./pages/HomePage";
 import './App.css'
 
 // Create the QueryClient instance
@@ -16,8 +16,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/signup" element={<SignupPage />} />
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/LoginPage" element={<LoginPage />} />
+          <Route path="/" element={<HomePage />} />
         </Routes>
       </Router>
     </QueryClientProvider>
